8d29feed849bd1bb19bfee23366e7084fe0078fd
kandrews92 <kraigandrews1992@gmail.com>
2015-12-14 13:01:16 -0500

    added setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import py2exe
+
+setup(name='fourprobe', 
+		version='1.0',
+		description='Analysis program for excel sheet and four-probe measurements',
+		author='Kraig Andrews',
+		author_email='kraigandrews1992@gmail.com',
+		packages=['distutils, 'distutils.command', 'xlrd', 'xlwt', 'numpy',
+					'math', 'os', 'sys'
+		],
+	)
